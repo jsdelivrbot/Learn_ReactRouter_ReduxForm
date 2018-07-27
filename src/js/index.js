2803bda67eea2a0f6b0117dedaff3b661a3a3c77
bismarcko <noirefield@gmail.com>
2018-07-27 11:27:22 +0700

    Delete app and add component for SPA

--- src/js/index.js
+++ src/js/index.js
@@ -2,30 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers/index';
+import PostsIndex from './components/posts_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 const store = createStoreWithMiddleware(reducers);
-
-class Hello extends React.Component {
-    render() { return <div>Hello!</div>; }
-}
-
-class Goodbye extends React.Component {
-    render() { return <div>Good bye!</div>; }
-}
-
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
             <div>
-                First Hello?
-                <Route path="/hello" component={Hello}/>
-                <Route path="/goodbye" component={Goodbye}/>
+                <Route path="/" component={PostsIndex}/>
             </div>
         </BrowserRouter>
     </Provider>
